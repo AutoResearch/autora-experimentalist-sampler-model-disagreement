21ea4d27dda10b5ad4fc7b5aa5a1168db825a998
Chad C Williams <ccwillia@uvic.ca>
2023-05-16 10:52:13 -0700

    added test and changed authorship

--- tests/test_model_disagreement_sampler.py
+++ tests/test_model_disagreement_sampler.py
@@ -1,13 +1,23 @@
-from autora.experimentalist.sampler.model_disagreement_sampler import example_sampler
+from autora.experimentalist.sampler.model_disagreement import model_disagreement_sampler
+from autora.skl.bms import BMSRegressor
+from autora.skl.darts import DARTSRegressor
 import numpy as np
 
 def test_output_dimensions():
-    X = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
-    n = 2
-    X_new = example_sampler(X, n)
+    #Meta-Setup
+    X = np.linspace(start=-3, stop=6, num=10).reshape(-1, 1)
+    y = (X**2).reshape(-1, 1)
+    n = 5
+    
+    #Theorists
+    bms_theorist = BMSRegressor()
+    darts_theorist = DARTSRegressor()
+    
+    bms_theorist.fit(X,y)
+    darts_theorist.fit(X,y)
+
+    #Sampler
+    X_new = model_disagreement_sampler(X, [bms_theorist, darts_theorist], n)
 
     # Check that the sampler returns n experiment conditions
     assert X_new.shape == (n, X.shape[1])
-
-
-# Note: We encourage you to adjust this test and write more tests.
