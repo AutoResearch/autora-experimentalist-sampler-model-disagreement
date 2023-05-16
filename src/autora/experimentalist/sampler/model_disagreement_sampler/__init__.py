83eedbb55e8ef8504ba22f35b787b9c1c8d6a95f
Chad C Williams <ccwillia@uvic.ca>
2023-05-16 13:41:47 -0700

    Update sampler location

--- src/autora/experimentalist/sampler/model_disagreement_sampler/__init__.py
+++ src/autora/experimentalist/sampler/model_disagreement_sampler/__init__.py
@@ -1,35 +1,64 @@
-"""
-Example Experimentalist Sampler
-"""
-
-
+import itertools
+from typing import Iterable, List
 import numpy as np
-from typing import Optional
 
-def example_sampler(
-    condition_pool: np.ndarray, num_samples: Optional[int] = None) -> np.ndarray:
+def model_disagreement_sampler(X: np.array, models: List, num_samples: int = 1):
     """
-    Add a description of the sampler here.
+    A sampler that returns selected samples for independent variables
+    for which the models disagree the most in terms of their predictions.
 
     Args:
-        condition_pool: pool of experimental conditions to evaluate
-        num_samples: number of experimental conditions to select
+        X: pool of IV conditions to evaluate in terms of model disagreement
+        models: List of Scikit-learn (regression or classification) models to compare
+        num_samples: number of samples to select
 
-    Returns:
-        Sampled pool of experimental conditions
-
-    *Optional*
-    Examples:
-        These examples add documentation and also work as tests
-        >>> example_sampler([1, 2, 3, 4])
-        1
-        >>> example_sampler(range(3, 10))
-        3
-
+    Returns: Sampled pool
     """
-    if num_samples is None:
-        num_samples = condition_pool.shape[0]
 
-    new_conditions = condition_pool
+    if isinstance(X, Iterable):
+        X = np.array(list(X))
 
-    return new_conditions[:num_samples]
+    X_predict = np.array(X)
+    if len(X_predict.shape) == 1:
+        X_predict = X_predict.reshape(-1, 1)
+
+    model_disagreement = list()
+
+    # collect diagreements for each model pair
+    for model_a, model_b in itertools.combinations(models, 2):
+
+        # determine the prediction method
+        if hasattr(model_a, "predict_proba") and hasattr(model_b, "predict_proba"):
+            model_a_predict = model_a.predict_proba
+            model_b_predict = model_b.predict_proba
+        elif hasattr(model_a, "predict") and hasattr(model_b, "predict"):
+            model_a_predict = model_a.predict
+            model_b_predict = model_b.predict
+        else:
+            raise AttributeError(
+                "Models must both have `predict_proba` or `predict` method."
+            )
+
+        # get predictions from both models
+        y_a = model_a_predict(X_predict)
+        y_b = model_b_predict(X_predict)
+
+        assert y_a.shape == y_b.shape, "Models must have same output shape."
+
+        # determine the disagreement between the two models in terms of mean-squared error
+        if len(y_a.shape) == 1:
+            disagreement = (y_a - y_b) ** 2
+        else:
+            disagreement = np.mean((y_a - y_b) ** 2, axis=1)
+
+        model_disagreement.append(disagreement)
+
+    assert len(model_disagreement) >= 1, "No disagreements to compare."
+
+    # sum up all model disagreements
+    summed_disagreement = np.sum(model_disagreement, axis=0)
+
+    # sort the summed disagreements and select the top n
+    idx = (-summed_disagreement).argsort()[:num_samples]
+
+    return X[idx]
